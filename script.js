6cf0ec3913baec4b94cd3b51bf785b985d4415ba
Ana Beatriz Almeida <anab.baldow@gmail.com>
2022-09-08 16:05:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-function calcular(){
-    let nota1 = Number(window.document.getElementById('nota1').value)
-    let nota2 = Number(window.document.getElementById('nota2').value)
-    let nota3 = Number(window.document.getElementById('nota3').value)
-    let res = window.document.getElementById('resultado')
-      
-  
-    let notaFinalArredondada = (nota1*0.2+nota2*0.3+nota3*0.5)
-    let notaFinal = notaFinalArredondada.toFixed(2)
-       
+function converter(){
 
-    if (notaFinal<60.00){
-        res.innerHTML = `Sua nota final é ${notaFinal}. Você está <b>REPROVADO</b>.`
-    } else{
-        res.innerHTML = `Sua nota final é ${notaFinal}. Você está <b>APROVADO</b>.`
-    }
-    
+    let valorReal = Number(window.document.getElementById("valor").value) 
+    let opcao = window.document.getElementById('opcao')
+    let res = window.document.getElementById('resultado')
+    let  cotacaoDolar = 5.50
+    let valorConvertido = (valorReal*cotacaoDolar).toFixed(2)
+
+    res.innerHTML=(`O preço do produto é <b>U$${valorConvertido}</b>.`)
+
 }
 
 
