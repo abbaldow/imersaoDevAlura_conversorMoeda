52667b2eae166e705c853e7b709d6039d78551b0
Ana Beatriz Almeida <anab.baldow@gmail.com>
2022-09-09 15:23:13 -0300

    Update script.js
    Desenvolvimento da lógica para a o usuário ser capaz de escolher o tipo de conversão desejada, de real para dolar, ou de dolar para real. Ainda não está funcionando.

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ function converter(){
     let opcao = window.document.getElementById('opcao')
     let res = window.document.getElementById('resultado')
     let  cotacaoDolar = 5.50
-    let valorConvertido = (valorReal*cotacaoDolar).toFixed(2)
-
-    res.innerHTML=(`O preço do produto é <b>U$${valorConvertido}</b>.`)
-
-}
-
+    let select = document.querySelector('input[name="select"]:checked').value
+    
+    if (select=='option-1'){
+        let valorConvertido = (valorReal/cotacaoDolar).toFixed(2)
+        res.innerHTML=(`O preço do produto é <b>U$${valorConvertido}</b>.`)
+    } else{
+        let valorConvertido = (valorReal*cotacaoDolar).toFixed(2)
+        res.innerHTML=(`O preço do produto é <b>R$${valorConvertido}</b>.`)
+    }
+    
+    }
 
 
