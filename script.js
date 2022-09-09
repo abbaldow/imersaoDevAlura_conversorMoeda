3ef9b05e52bb3bea06547029535a1e0c2af32d2f
Ana Beatriz Almeida <anab.baldow@gmail.com>
2022-09-09 15:27:10 -0300

    Update script.js
    alteração na lógica de seleção do valor do radio

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function converter(){
     let valorReal = Number(window.document.getElementById("valor").value) 
     let opcao = window.document.getElementById('opcao')
     let res = window.document.getElementById('resultado')
-    let  cotacaoDolar = 5.50
-    let select = document.querySelector('input[name="select"]:checked').value
+    let cotacaoDolar = 5.50
+    let select = document.querySelector('.select-1:checked').value
     
-    if (select=='option-1'){
+    if (select==true){
         let valorConvertido = (valorReal/cotacaoDolar).toFixed(2)
         res.innerHTML=(`O preço do produto é <b>U$${valorConvertido}</b>.`)
     } else{
@@ -15,5 +15,5 @@ function converter(){
     }
     
     }
-
+ console.log(select)
 
